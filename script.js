9532b6d8e1e7fe296c765cbf0d7043fdec8edf2c
Rohtash2021 <125363372+Rohtash2021@users.noreply.github.com>
2023-08-22 16:32:07 +0530

    script.js

--- script.js
+++ script.js
@@ -3,10 +3,6 @@ const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
 
-function reload() {
-    window.location.reload();
-}
-
 async function fetchNews(query) {
     const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
     const data = await res.json();
